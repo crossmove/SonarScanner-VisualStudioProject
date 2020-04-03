1f415fce06156708a1e737d447be8cbbde0fcba0
pradeep <pradeep.exe@gmail.com>
2020-04-03 20:02:54 +0530

    first commit

---
+++ Car/CarClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    public class CarClass
+    {       
+        public int Speed()
+        {
+            int a = 1;
+            int b = 2;
+            return a + b;
+        }
+
+        public int Gear()
+        {
+            int c = 1;
+            int d = 2;
+            return c - d;
+        }
+
+        public int Brake()
+        {
+            int e = 1;
+            int f = 2;
+            return e * f;
+        }
+
+        public int Brake1()
+        {
+            int g = 1;
+            int h = 2;
+            return g / h;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Car;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       
+        [TestMethod]
+        public void BrakeMethod3()
+        {
+            CarClass car = new CarClass();            
+            //Fail
+            Assert.AreEqual(3, car.Speed());
+        }
+    }
+}
